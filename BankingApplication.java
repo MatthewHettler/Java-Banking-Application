a60a2b383e1fc4b9f590478d061e89c1bf73870d
Matthew Hettler <Matthew.R.Hettler@gmail.com>
2020-06-25 12:10:34 -0500

    added a class of BankAccount to the file.

--- BankingApplication.java
+++ BankingApplication.java
@@ -4,6 +4,38 @@ public class BankingApplication {
 
     public static void main(String[] args) {
         
+
+
+
     }
+
+}
+
+class BankAccount {
+
+    int balance;
+    int previousTransaction;
+    String customerName;
+    String customerId;
+
+    void deposit(int amount)
+    {
+        if (amount != 0){
+            balance = balance + amount;
+            previousTransaction = amount;
+        }
+
+        void withdraw(int amount){
+            if (amount !=0){
+                balance = balance - amount;
+                previousTransaction = -amount;
+            }
+        }
+
+
+
+    }
+
+
 
 }
